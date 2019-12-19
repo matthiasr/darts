5a10d104f6aefd6eee5c29c332b039905bca26a5
Matthias Rampke <matthias@rampke.de>
2019-12-19 14:56:37 +0100

    run 1 million times and collect results

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ import (
 )
 
 const (
-	runs = 100
+	runs = 1e6
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	s := summary{}
+	start := time.Now()
 	for i := 0; i < runs; i++ {
-		fmt.Printf("%d ", NewGame().Run())
+		score := NewGame().Run()
+		s.Observe(score)
 	}
+	end := time.Now()
+
+	fmt.Println("Summary:", s)
+	fmt.Println("Average:", s.Average())
+	fmt.Println("Duration:", end.Sub(start))
 }
